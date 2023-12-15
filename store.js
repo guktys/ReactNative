07ff446939043f5ac000d485154e3212ffc02bea
Evgenia Deken <guk.genia@gmail.com>
2023-12-15 04:05:30 +0200

    Запрацювала асинхроне збереження

--- store.js
+++ store.js
@@ -1,21 +1,25 @@
 import { createStore, combineReducers } from 'redux';
 import newsReducer from './store/reducers/newsReducer';
-import {persistReducer, persistStore} from "redux-persist";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import stringReducer from "./store/reducers/stringReducer";
+import { persistReducer, persistStore } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import stringReducer from './store/reducers/stringReducer';
 
 const rootReducer = combineReducers({
     news: newsReducer,
     string: stringReducer,
 });
 
-
 const persistConfig = {
     key: 'root',
     storage: AsyncStorage,
-    whitelist: ['string'], // Только 'string' будет сохраняться
+    whitelist: ['string'], // Будет сохраняться только 'string'
 };
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-const store = createStore(rootReducer);
+
+// Используйте persistedReducer вместо rootReducer
+const store = createStore(persistedReducer);
+
 export const persistor = persistStore(store);
+
 export default store;
